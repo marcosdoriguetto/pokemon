2750f36d3e31b10981dc332004ef194c2abe85d5
Marcos <marcosdoriguetto@gmail.com>
2021-07-17 21:49:40 -0300

    Criação da função de buscar pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -27,45 +27,71 @@ type PokemonType = {
 
 export function App() {
   const [pokemon, setPokemon] = useState<PokemonType[]>([]);
+  const [allPokemons, setAllPokemons] = useState<PokemonType[]>([]);
   const [nextUrl, setNextUrl] = useState('');
   const [previousUrl, setPreviousUrl] = useState('');
+  const [valueSearch, setValueSearch] = useState('');
 
   const initialUrlApi = 'https://pokeapi.co/api/v2/pokemon?limit=25&offset=0'
+  const initialUrlApiGetAllPokemons = 'https://pokeapi.co/api/v2/pokemon?limit=1118&offset=0'
 
   useEffect(() => {
+    async function fetchApiAllPokemons() {
+      const getAllPokemons = await axios.get(initialUrlApiGetAllPokemons);
+
+      await savePokemons(getAllPokemons.data.results, 'all')
+    }
+    fetchApiAllPokemons()
     fetchApi(initialUrlApi)
   }, [])
 
   async function fetchApi(props: string) {
-    const getAllPokemons = await axios.get(props);
+    const getAllPokemonsPage = await axios.get(props);
 
-    setNextUrl(getAllPokemons.data.next)
-    setPreviousUrl(getAllPokemons.data.previous)
-    await savePokemons(getAllPokemons.data.results)
+    setNextUrl(getAllPokemonsPage.data.next)
+    setPreviousUrl(getAllPokemonsPage.data.previous)
+    await savePokemons(getAllPokemonsPage.data.results, '')
   }
 
-  const savePokemons: ([]) => void = async previous => {
+  const savePokemons: ([], actionType: string) => void = async (previous, actionType) => {
     let _pokemon: PokemonType[] = await Promise.all(previous.map(async pokemon => {
       let pokemonRegister = await axios.get(pokemon.url)
       return pokemonRegister
     }))
 
-    setPokemon(_pokemon)
+    actionType === 'all' ? setAllPokemons(_pokemon) : setPokemon(_pokemon)
   }
 
   return (
     <Content>
-      <ContentButton>
-        <Button next={false} disabled={previousUrl === null ? true : false} onClick={() => fetchApi(previousUrl)}>Previous</Button>
-        <Button disabled={nextUrl === null ? true : false} onClick={() => fetchApi(nextUrl)}>Next</Button>
-      </ContentButton>
-      {
-        pokemon.map(pokemon => {
+      <input type="text" onChange={event => setValueSearch(event.target.value)} />
+      {valueSearch === "" ? (
+        <>
+          {
+            pokemon.map(pokemon => {
+              return (
+                <Pokemon key={pokemon.data.id} name={pokemon.data.name} types={pokemon.data.types} sprites={pokemon.data.sprites.front_default} />
+              )
+            })
+          }
+          <ContentButton>
+            <Button next={false} disabled={previousUrl === null ? true : false} onClick={() => fetchApi(previousUrl)}>Previous</Button>
+            <Button disabled={nextUrl === null ? true : false} onClick={() => fetchApi(nextUrl)}>Next</Button>
+          </ContentButton>
+        </>
+      ) : (
+        allPokemons.filter(value => {
+          if (value.data.name.toLowerCase().includes(valueSearch.toLowerCase())) {
+            return value
+          }
+        }).map(pokemon => {
           return (
             <Pokemon key={pokemon.data.id} name={pokemon.data.name} types={pokemon.data.types} sprites={pokemon.data.sprites.front_default} />
           )
         })
+      )
       }
+
     </Content>
   )
 }
